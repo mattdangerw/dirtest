67d6350df7c4fb47bc4700273a32abc9b5855e61
Matt Watson <mattdangerw@gmail.com>
2024-02-01 16:54:50 -0800

    Extend search path

--- keras/__init__.py
+++ keras/__init__.py
@@ -1,3 +1,10 @@
+import os
+
+# Import everything from /api/ into keras.
 from keras.api import *
 
+# Add everything in /api/ to the module search path.
+__path__.append(os.path.join(os.path.dirname(__file__), "api"))
+
+# Define a version string.
 __version__ = "3.0.4"
