7928643e46a803931b78a852f3869bb4d8701d6a
Matt Watson <mattdangerw@gmail.com>
2024-04-02 11:42:10 -0700

    try to clean namespace

--- keras/__init__.py
+++ keras/__init__.py
@@ -1,10 +1,16 @@
-import os
-
 # Import everything from /api/ into keras.
 from keras.api import *
 
 # Add everything in /api/ to the module search path.
+import os
 __path__.append(os.path.join(os.path.dirname(__file__), "api"))
+del os
 
 # Define a version string.
 __version__ = "3.0.5"
+
+def __dir__():
+    keys = dict.fromkeys((globals().keys()))
+    keys.pop("src")
+    keys.pop("api")
+    return list(keys)
